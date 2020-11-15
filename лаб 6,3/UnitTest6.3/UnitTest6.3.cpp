6b440a676cf143c81e7afe038a7d391afa0bd083
Iryna Prendkovych <iryna.prendkovych.itis.2020@lpnu.ua>
2020-11-15 16:37:03 +0200

    lab 6.3

---
+++ лаб 6,3/UnitTest6.3/UnitTest6.3.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../лаб 6,3/лаб 6,3.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest63
+{
+	TEST_CLASS(UnitTest63)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+
+			int t;
+			const int n = 4;
+			int r[n] = { 20, 1, -5, 6 };
+			t = Sum(r, n);
+			Assert::AreEqual(t, 7);
+
+		}
+	};
+}
---
+++ лаб 6,3/лаб 6,3/лаб 6,3.cpp
@@ -0,0 +1,63 @@
+﻿// лаб 6,3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+
+using namespace std;
+void Create(int* a, const int size, const int Low, const int High)
+{
+	for (int i = 0; i < size; i++)
+		a[i] = Low + rand() % (High - Low + 1);
+}
+void Print(int* a, const int size)
+{
+	for (int i = 0; i < size; i++)
+		cout << setw(4) << a[i];
+	cout << endl;
+}
+void Inverse(int* a, const int n)
+{
+	int tmp;
+	for (int i = 0; i < n / 2; i++)
+	{
+		tmp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = tmp;
+	}
+}
+int Sum(int* r, const int size)
+{
+	int j = 0;
+	int S = 0;
+	for (int i = 0; i < size; i++)
+		if ((r[i] > 0) && (r[i] % 4 != 0))
+			S += r[i];
+	return S;
+}
+
+int main()
+{
+	srand((unsigned)time(NULL)); 
+	const int n = 9;
+	int a[n];
+	int Low = -10;
+	int High = 10;
+	Create(a, n, Low, High);
+	Print(a, n);
+	Inverse(a, n);
+	Print(a, n);
+	return 0;
+}
+
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
